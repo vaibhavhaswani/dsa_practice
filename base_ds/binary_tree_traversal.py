f768b9fff5f6a516ae343e8bcd47ca6d2370cc6d
Vaibhav Haswani <vaibhavhaswani@gmail.com>
2025-01-28 20:17:49 +0000

    advance tree methods added

--- base_ds/binary_tree_traversal.py
+++ base_ds/binary_tree_traversal.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class Node:
     def __init__(self,data):
         self.data=data
@@ -31,10 +34,116 @@ class BT:
         self.postorder(node.left)
         self.postorder(node.right)
         print(node.data,end=" > ")
+    
+    # breadh first search \ level order
+    def levelorder(self,node):
+        if node is None:
+            print("Tree is Empty!")
+            return 
+        queue=deque([node])  # 0
+        while queue:
+            current=queue.popleft()           # 0 popped 1 popped
+            print(current.data,end=" > ")
+            
+            if current.left:
+                queue.append(current.left)   # [1] [2,3]
+            if current.right:
+                queue.append(current.right)  # [1,2] [2,3,4]
 
-    # level order traversal : todo
-    # tree height : todo
+        # breadh first search \ level order without queue
+    def levelorder(self,node):
+        if node is None:
+            print("Tree is Empty!")
+            return 
+        queue=[node] # 0
+        while queue:
+            current=queue.pop(0)           # 0 popped 1 popped
+            print(current.data,end=" > ")
+            
+            if current.left:
+                queue.append(current.left)   # [1] [2,3]
+            if current.right:
+                queue.append(current.right)  # [1,2] [2,3,4]
+    
+    def _getheight(self):
+        if self.root is None:
+            print("Tree is Empty!")
+            return
+        
+        queue=deque([self.root])
+        i=0
+        left=[]
+        right=[]
+        while queue:
+            current=queue.popleft()
+
+            if current.left:
+                left.append(current.left)
+                queue.append(current.left)
+            if current.right:
+                right.append(current.right)
+                queue.append(current.right)
+
+            
+        return max(len(left),len(right))
+        
+    # get tree height through recursion
+    def getheight(self,node): #optimal
+        if node is None:
+            return 0
+        left=self.getheight(node.left)
+        right=self.getheight(node.right)
+        
+        return max(left,right)+1
+    
     # tree constructor based on given data and selected traversal
+    @classmethod
+    def buildtree(self,traversal:dict):
+        #if inorder and preorder list are given
+        if 'inorder' in list(traversal.keys()) and 'preorder' in list(traversal.keys()):
+            pre=traversal['preorder'] # preorder traversal , root in the beginning
+            ino=traversal['inorder'] # inorder traversal has root in the middle of left and right
+
+            if not pre or not ino:
+                return None
+
+            root_data=pre.pop(0)
+            root=Node(root_data)
+
+            root_index=ino.index(root_data)
+            ino_left,ino_right=ino[:root_index],ino[root_index+1:]
+
+            root.left=self.buildtree({"inorder":ino_left,"preorder":pre})
+            root.right=self.buildtree({"inorder":ino_right,"preorder":pre})
+
+            return root
+        
+        #if inorder and post order list are given
+        elif 'inorder' in list(traversal.keys()) and 'postorder' in list(traversal.keys()):
+            post=traversal['postorder'] # postorder traversal , root in the end
+            ino=traversal['inorder'] # inorder traversal has root in the middle of left and right
+
+            if not post or not ino:
+                return None
+            
+            root_data=post.pop()
+            root=Node(root_data)
+
+            root_index=ino.index(root_data)
+
+            ino_left,ino_right=ino[:root_index],ino[root_index+1:]
+
+            # right tree will be built first as of post order (reverse)
+            root.right=self.buildtree(traversal={"inorder":ino_right,"postorder":post})
+            root.left=self.buildtree(traversal={"inorder":ino_left,"postorder":post})
+
+            return root
+        
+        #if levelorder list is given
+        elif 'levelorder' in list(traversal.keys()):
+            pass
+        return None
+
     
 if __name__=="__main__":
     bt=BT()
@@ -45,10 +154,26 @@ if __name__=="__main__":
     bt.root.left.right=Node(4)
     bt.root.right.left=Node(5)
     bt.root.right.right=Node(6)
+    bt.root.right.right.left=Node(8)
 
     bt.inorder(bt.root)
     print()
     bt.preorder(bt.root)
     print()
     bt.postorder(bt.root)
+    print()
+    bt.levelorder(bt.root)
+    print()
+    print(f"Tree height: {bt._getheight()}")
+    print(f"Tree height: {bt.getheight(bt.root)}")
+
+    # tree_dict={"inorder":[4,2,5,1,6,3,7],"preorder":[1,2,4,5,3,6,7]}
+    tree_dict={"inorder":[4,2,5,1,6,3,7],"postorder":[4,5,2,6,7,3,1]}
+
+    root=BT.buildtree(traversal=tree_dict)
+    tree=BT()
+    tree.root=root
+    tree.preorder(tree.root)
+    print()
+    tree.levelorder(tree.root)
     print()
